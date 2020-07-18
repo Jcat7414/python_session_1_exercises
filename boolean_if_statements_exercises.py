7a05cd4fa3a66074bf294192978f8bd2472d97a3
Joy Taylor <jcat7414@gmail.com>
2020-07-18 22:46:16 +1000

    Boolean and If Statement Exercises

---
+++ boolean_if_statements_exercises.py
@@ -0,0 +1,88 @@
+print("Q1")
+
+print("Is it time for Roary to catch moths?")
+
+moths_in_house = True
+
+print("Get the moths!")
+
+moths_in_house = False
+
+print("No threats detected.")
+
+print("")
+
+print("Q2")
+
+print("Is it time for Roary to go moth hunting?")
+
+moths_in_house = True
+mitch_is_home = True
+
+print("Hooman! Help me get the moths!")
+
+moths_in_house = False
+mitch_is_home = False
+
+print("No threats detected.")
+
+moths_in_house = True
+mitch_is_home = False
+
+print("Meooooooow! Hissssss!")
+
+moths_in_house = False
+mitch_is_home = True
+
+print("Climb on Mitch.")
+
+print("")
+
+print("Q3")
+
+light_colour = "Red"
+car_detected = False
+
+print("Do nothing.")
+
+light_colour = "Red"
+car_detected = True
+
+print("Flash!")
+
+light_colour = "Green"
+car_detected = False
+
+print("Do nothing.")
+
+light_colour = "Green"
+car_detected = True
+
+print("Do nothing.")
+
+light_colour = "Amber"
+car_detected = False
+
+print("Do nothing.")
+
+light_colour = "Amber"
+car_detected = True
+
+print("Do nothing.")
+
+print("")
+
+print("Q4")
+
+print("Find out if you are tall enough to ride the rollercoaster!")
+
+height_cm = input("Enter your height in centimeters: ")
+
+result = int(height_cm)
+
+if result >= 120:
+    print("Hop on!")
+elif result<120:
+    print("Sorry, not today :(")
+else:
+    print("Hop on!")
